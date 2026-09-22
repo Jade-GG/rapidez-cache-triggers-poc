d49f04a463bdbec5d11587056e8cd12b5f9d8558
Jade <jade@justbetter.nl>
2026-09-22 13:30:14 +0200

    use loadMigrationsFrom instead

--- src/RapidezCacheTriggersPocServiceProvider.php
+++ src/RapidezCacheTriggersPocServiceProvider.php
@@ -14,18 +14,22 @@ class RapidezCacheTriggersPocServiceProvider extends ServiceProvider
     public function boot()
     {
         $this
+            ->bootMigrations()
             ->bootPublishables();
     }
 
-    public function bootPublishables() : self
+    public function bootMigrations(): static
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+        return $this;
+    }
+
+    public function bootPublishables(): static
     {
         $this->publishes([
             __DIR__.'/../config/rapidez/cache-triggers-poc.php' => config_path('rapidez/cache-triggers-poc.php'),
         ], 'rapidez-cache-triggers-poc-config');
-
-        $this->publishesMigrations([
-            __DIR__.'/../database/migrations' => database_path('migrations'),
-        ]);
 
         return $this;
     }
